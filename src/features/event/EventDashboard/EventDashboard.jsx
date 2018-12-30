1e7c5508ce0b2e474fbddc6693da8dfc355355ef
WonjeongPark <bnhs1127@gmail.com>
2018-12-30 10:27:42 +0900

    Form(uncontrolled,controlled, submitting)-section 4

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
-import { Grid, Button } from 'semantic-ui-react'
-import EventList from '../EventList/EventList'
-import EventForm from '../EventForm/EventForm'
+import React, { Component } from 'react';
+import { Grid, Button } from 'semantic-ui-react';
+import cuid from 'cuid';
+import EventList from '../EventList/EventList';
+import EventForm from '../EventForm/EventForm';
 
 //s_3.7.1
 const eventsDashboard = [
@@ -73,6 +74,15 @@ class EventDashboard extends Component {
     })
   }
 
+  handleCreateEvent = (newEvent) => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = '/assets/user.png';
+    const updatedEvents = [...this.state.events, newEvent];
+    this.setState({
+      events : updatedEvents,
+      inOpen: false
+    })
+  }
 
   render() {
     return (
@@ -83,7 +93,7 @@ class EventDashboard extends Component {
         <Grid.Column  width={6}>
           <Button onClick={this.handleFormOpen} positive content='Create Event'/>
           {this.state.isOpen &&
-            <EventForm handleCancel={this.handleCancel}/>}
+            <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleCancel}/>}
         </Grid.Column>
       </Grid>
     )
