11886bac420e684f6c7ac0138ae0367a901e172e
WonjeongPark <bnhs1127@gmail.com>
2019-02-18 19:03:22 +0900

    End of section 6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -18,20 +18,21 @@ class App extends Component {
         <Switch>
           <Route  exact path='/' component={HomePage} />        
         </Switch>
-        <Route path="/(.+)" render={() => (
-          <div>
-          <NavBar />
-          <Container className="main">
-            <Switch>
-              <Route path='/events' component={EventDashboard} />
-              <Route path='/event/:id' component={EventDetailedPage} />
-              <Route path='/people' component={PeopleDashboard} />
-              <Route path='/profile/:id' component={UserDetailedPage} />
-              <Route path='/settings' component={SettingsDashboard} />
-              <Route path='/createEvent' component={EventForm} />
-            </Switch>
-          </Container>
-          </div>          
+        <Route
+          path="/(.+)" render={() => (
+            <div>
+            <NavBar />
+            <Container className="main">
+              <Switch>
+                <Route path='/events' component={EventDashboard} />
+                <Route path='/event/:id' component={EventDetailedPage} />
+                <Route path='/people' component={PeopleDashboard} />
+                <Route path='/profile/:id' component={UserDetailedPage} />
+                <Route path='/settings' component={SettingsDashboard} />
+                <Route path='/createEvent' component={EventForm} />
+              </Switch>
+            </Container>
+            </div>          
         )}
         />
       </div>
