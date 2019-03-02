b1bbec6b8e63ab14a8537f0cc73f9d7f7f45b048
WonjeongPark <bnhs1127@gmail.com>
2019-03-02 15:57:58 +0900

    End of section 7

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import { Provider } from 'react-redux'
 import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './app/layout/App';
-import * as serviceWorker from './serviceWorker';
 import { configureStore } from './app/store/configureStore';
 
-const store = configureStore;
+const store = configureStore();
 
 const rootEl = document.getElementById('root');
 
@@ -31,12 +30,3 @@ if (module.hot) {
 
 render();
 
-// serviceWorker();
-
-
-//ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
